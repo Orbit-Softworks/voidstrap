c2f1f0b09ac7e1979c19d5595a57c89ef8291568
Smilez <smilezreal@yahoo.com>
2025-10-17 18:10:53 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -103,9 +103,9 @@ export default function BloxstrapClone() {
         {/* Open Source Section */}
         <div className="animate-on-scroll opacity-0 translate-y-16 transition-all duration-1000 ease-out py-8 flex flex-col items-center justify-center gap-x-12 gap-y-6 lg:flex-row">
           <div className="flex-1">
-            <h1 className="text-2xl font-medium mb-2">Free and open source</h1>
+            <h1 className="text-2xl font-medium mb-2">Voidstraps Redistribution Policy</h1>
             <p className="text-gray-200 mb-2">
-              Voidstrap's code is not available on GitHub and no one can see has permission to contribute to it, or even make their own version.
+              Voidstrap's code is publicly available on GitHub to prove it's safe and virus-free. However, using our code in other projects violates our policies and will result in a permanent ban.
             </p>
             <p>
               <a className="text-violet-400 hover:text-violet-300 transition duration-300" href="Uh, Uh, Uh Skids">
